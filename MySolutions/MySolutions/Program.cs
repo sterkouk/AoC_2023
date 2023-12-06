2d88d47ad979ec16466b4e51981d4c2d2782add2
Stergios Koukouftopoulos <stergios06@hotmail.com>
2023-12-06 22:59:02 +0200

    Adding first solution

---
+++ MySolutions/MySolutions/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Solutions solution = new Solutions();
+int sum = solution.Solution1();
+Console.WriteLine(sum);
---
+++ MySolutions/MySolutions/Solutions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Solutions
+{
+	public int Solution1()
+	{
+        string initial;
+
+        char first;
+        char last;
+        int sum = 0;
+
+        foreach (string line in File.ReadLines(@"D:\VSProjects\AoC\MySolutions\MySolutions\input.txt"))
+        {
+
+            if (line.Any(c => char.IsDigit(c)))
+            {
+                //Console.WriteLine(line);
+                // Extract digits from the beginning of the line and end
+                initial = new string(line.Where(Char.IsDigit).ToArray());
+
+                first = initial.First();
+                last = initial.Last();
+
+                int number = int.Parse(first.ToString() + last.ToString());
+
+                sum = sum + number;
+            }
+        }
+
+        return sum;
+
+    }
+}
